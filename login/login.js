1efc37c49293a3256b09f8f11fa54ef0b0763f47
dbsgur <kkr431@naver.com>
2020-12-21 16:03:43 +0900

    Input Login

--- login/login.js
+++ login/login.js
@@ -13,34 +13,39 @@ import {
   ScrollView,
   View,
   Text,
-  TouchableOpacity ,
+  TouchableOpacity,
   Button,
   TextInput,
   Image,
+  TouchableWithoutFeedback,
+  Keyboard,
+  Dimensions,
 } from 'react-native';
-import { withNavigation } from 'react-navigation';
+import {withNavigation} from 'react-navigation';
 import Swiper from 'react-native-swiper';
 import AsyncStorage from '@react-native-community/async-storage';
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
+import {getStatusBarHeight} from 'react-native-status-bar-height';
+import {getBottomSpace} from 'react-native-iphone-x-helper';
 const func = require('../server/api');
 
- class Login extends React.Component{
-  constructor(props){
+class Login extends React.Component {
+  constructor(props) {
     super(props);
-    this.state={
-      name1: "",
-      pass: ""
-    }
+    this.state = {
+      name1: '',
+      pass: '',
+    };
   }
-  componentWillMount(){
+  componentWillMount() {
     // console.log(func.api(3001,'login'));
     AsyncStorage.getItem('login_onoff_set', (err, result) => {
-      if(result === 'true'){
-        this.props.navigation.navigate('Main')
-      }else{
-        this.props.navigation.navigate('Login')
+      if (result === 'true') {
+        this.props.navigation.navigate('Main');
+      } else {
+        this.props.navigation.navigate('Login');
       }
-    })
+    });
   }
 
   handleName = (e) => {
@@ -55,17 +60,17 @@ const func = require('../server/api');
     });
     // console.log(this.state.pass);
   };
-  
-  onlogin=(e)=>{
+
+  onlogin = (e) => {
     const post = {
       id: this.state.name1,
       passwd: this.state.pass,
     };
 
-    fetch(func.api(3001,'login'), {
-      method: "post",
+    fetch(func.api(3001, 'login'), {
+      method: 'post',
       headers: {
-        "content-type": "application/json",
+        'content-type': 'application/json',
       },
       body: JSON.stringify(post),
     })
@@ -73,148 +78,312 @@ const func = require('../server/api');
       .then((json) => {
         if (json) {
           AsyncStorage.setItem('login_onoff_set', 'true', () => {
-            AsyncStorage.setItem('login_user_info', 
+            AsyncStorage.setItem(
+              'login_user_info',
               JSON.stringify({
-                'login_set': 'true',
-                'user_key': json.user_key,
-                'user_id': json.user_id,
-                'user_sex': json.user_sex,
-                'user_email': json.user_email,
-                'user_deptno': json.user_deptno,
-                'user_stdno': json.user_stdno,
-                'user_nickname': json.user_nickname,
-              }), () => {
-              // console.log('로그인 정보 저장')
+                login_set: 'true',
+                user_key: json.user_key,
+                user_id: json.user_id,
+                user_sex: json.user_sex,
+                user_email: json.user_email,
+                user_deptno: json.user_deptno,
+                user_stdno: json.user_stdno,
+                user_nickname: json.user_nickname,
+              }),
+              () => {
+                // console.log('로그인 정보 저장')
 
-              this.props.navigation.navigate('Main')
-            })
+                this.props.navigation.navigate('Main');
+              },
+            );
           });
         } else {
-          alert("아이디 또는 비밀번호 틀림")
+          alert('아이디 또는 비밀번호 틀림');
         }
       });
-  }
+  };
 
   singupBtn = (e) => {
     e.preventDefault();
-    this.props.navigation.navigate('Signup')
+    this.props.navigation.navigate('Signup');
   };
 
   find_idpwBtn = (e) => {
     e.preventDefault();
-    this.props.navigation.navigate('Find_idpw')
+    this.props.navigation.navigate('Find_idpw');
   };
 
-  render(){
-    return(
+  render() {
+    let screenHeight =
+      Dimensions.get('window').height - getStatusBarHeight() - getBottomSpace();
+    return (
       <SafeAreaView style={styles.White_login}>
-          <View style={styles.bg1}>
-            <View style={styles.Textbox_login2}>
-              <Text style={styles.Intro_login}>창원대 매칭앱</Text>
-            </View>
-            <View style={styles.Textbox_login}>
-              <Text style={styles.Intro_login2}>와글와글</Text>
-            </View>
-          </View>
+        <KeyboardAwareScrollView>
+          <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <View
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                height: screenHeight,
+              }}>
+              <View style={styles.bg1}>
+                <View style={styles.Textbox_login2}>
+                  <Text style={styles.Intro_login}>창원대 매칭앱</Text>
+                </View>
+                <View style={styles.Textbox_login}>
+                  <Text style={styles.Intro_login2}>와글와글</Text>
+                </View>
+              </View>
 
-          <View style={styles.Swiper_bg}>
-            <Swiper style={styles.wrapper} autoplay={true} autoplayTimeout={3} 
-              activeDot={
-                <View
-                  style={{
-                    backgroundColor: '#f05052',
-                    width: 9,
-                    height: 9,
-                    borderRadius: 7,
-                    marginLeft: 7,
-                    marginRight: 7
-                  }}
-                />
-              }>
+              <View style={styles.Swiper_bg}>
+                <Swiper
+                  style={styles.wrapper}
+                  autoplay={true}
+                  autoplayTimeout={3}
+                  activeDot={
+                    <View
+                      style={{
+                        backgroundColor: '#f05052',
+                        width: 9,
+                        height: 9,
+                        borderRadius: 7,
+                        marginLeft: 7,
+                        marginRight: 7,
+                      }}
+                    />
+                  }>
+                  <View style={styles.slide1}>
+                    <Text style={styles.swiper_text}>
+                      '다른 학과' 사람들과 친해질수 있는
+                    </Text>
+                    <Text style={styles.swiper_text}>최고의 방법</Text>
+                    <Image
+                      style={styles.swiper_image}
+                      source={require('./swiper_image1.png')}
+                      resizeMode="cover"
+                    />
+                  </View>
+                  <View style={styles.slide2}>
+                    <Text style={styles.swiper_text}>창원대</Text>
+                    <Text style={styles.swiper_text}>창원대</Text>
+                    <Image
+                      style={styles.swiper_image}
+                      source={require('./swiper_image1.png')}
+                      resizeMode="cover"
+                    />
+                  </View>
+                  <View style={styles.slide3}>
+                    <Text style={styles.swiper_text}>와글와글</Text>
+                    <Text style={styles.swiper_text}>와글와글</Text>
+                    <Image
+                      style={styles.swiper_image}
+                      source={require('./swiper_image1.png')}
+                      resizeMode="cover"
+                    />
+                  </View>
+                </Swiper>
+              </View>
+
+              <View style={styles.bg2}>
+                <View style={styles.Text_login}>
+                  <Text style={styles.Text_login_text}>아이디</Text>
+                  <TextInput
+                    style={styles.Text_login_input}
+                    id="name"
+                    value={this.state.name1}
+                    onChangeText={this.handleName}
+                  />
+                </View>
+                <View style={styles.Text_login}>
+                  <Text style={styles.Text_login_text}>비밀번호</Text>
+                  <TextInput
+                    style={styles.Text_login_input2}
+                    id="pass"
+                    name="pass"
+                    value={this.state.pass}
+                    secureTextEntry={true}
+                    onChangeText={this.handleName2}
+                  />
+                </View>
+              </View>
+
+              <View style={styles.bg3}>
+                <View style={styles.login_button_view}>
+                  <TouchableOpacity
+                    style={{marginRight: 70}}
+                    onPress={this.singupBtn}>
+                    <Text style={{fontSize: 15, fontFamily: 'Jalnan'}}>
+                      처음이신가요?
+                    </Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    style={{marginRight: 20}}
+                    onPress={this.find_idpwBtn}>
+                    <Text style={{fontSize: 15, fontFamily: 'Jalnan'}}>
+                      ID/PW 찾기
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+
+              <View style={{width: '100%'}}>
+                <TouchableOpacity
+                  style={styles.Btn_login}
+                  onPress={this.onlogin}>
+                  <Text
+                    style={{
+                      color: 'white',
+                      fontFamily: 'Jalnan',
+                      fontSize: 20,
+                    }}>
+                    로그인
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </TouchableWithoutFeedback>
+        </KeyboardAwareScrollView>
+        {/* <View style={styles.bg1}>
+          <View style={styles.Textbox_login2}>
+            <Text style={styles.Intro_login}>창원대 매칭앱</Text>
+          </View>
+          <View style={styles.Textbox_login}>
+            <Text style={styles.Intro_login2}>와글와글</Text>
+          </View>
+        </View>
+
+        <View style={styles.Swiper_bg}>
+          <Swiper
+            style={styles.wrapper}
+            autoplay={true}
+            autoplayTimeout={3}
+            activeDot={
+              <View
+                style={{
+                  backgroundColor: '#f05052',
+                  width: 9,
+                  height: 9,
+                  borderRadius: 7,
+                  marginLeft: 7,
+                  marginRight: 7,
+                }}
+              />
+            }>
             <View style={styles.slide1}>
-              <Text style={styles.swiper_text}>'다른 학과' 사람들과 친해질수 있는</Text>
+              <Text style={styles.swiper_text}>
+                '다른 학과' 사람들과 친해질수 있는
+              </Text>
               <Text style={styles.swiper_text}>최고의 방법</Text>
-              <Image style={styles.swiper_image} source={require('./swiper_image1.png')} resizeMode="cover"/>
+              <Image
+                style={styles.swiper_image}
+                source={require('./swiper_image1.png')}
+                resizeMode="cover"
+              />
             </View>
             <View style={styles.slide2}>
               <Text style={styles.swiper_text}>창원대</Text>
               <Text style={styles.swiper_text}>창원대</Text>
-              <Image style={styles.swiper_image} source={require('./swiper_image1.png')} resizeMode="cover"/>
+              <Image
+                style={styles.swiper_image}
+                source={require('./swiper_image1.png')}
+                resizeMode="cover"
+              />
             </View>
             <View style={styles.slide3}>
               <Text style={styles.swiper_text}>와글와글</Text>
               <Text style={styles.swiper_text}>와글와글</Text>
-              <Image style={styles.swiper_image} source={require('./swiper_image1.png')} resizeMode="cover"/>
+              <Image
+                style={styles.swiper_image}
+                source={require('./swiper_image1.png')}
+                resizeMode="cover"
+              />
             </View>
-            </Swiper>
-          </View>
+          </Swiper>
+        </View>
 
-          <View style={styles.bg2}>
-            <View style={styles.Text_login}>
-              <Text style={styles.Text_login_text}>아이디</Text>
-              <TextInput style={styles.Text_login_input} id="name"
-                value={this.state.name1}
-                onChangeText={this.handleName}/>
-            </View>
-            <View style={styles.Text_login}>
-              <Text style={styles.Text_login_text}>비밀번호</Text>
-              <TextInput style={styles.Text_login_input2}   id="pass"
-                name="pass"
-                value={this.state.pass}
-                secureTextEntry={true}
-                onChangeText={this.handleName2}/>
-            </View>
+        <View style={styles.bg2}>
+          <View style={styles.Text_login}>
+            <Text style={styles.Text_login_text}>아이디</Text>
+            <TextInput
+              style={styles.Text_login_input}
+              id="name"
+              value={this.state.name1}
+              onChangeText={this.handleName}
+            />
           </View>
-            
-          <View style={styles.bg3}>
-            <View style={styles.login_button_view}>
-              <TouchableOpacity style={{marginRight:70}} onPress={this.singupBtn}>
-                <Text style={{fontSize:15,fontFamily:'Jalnan'}}>처음이신가요?</Text>
-              </TouchableOpacity>
-              <TouchableOpacity  style={{marginRight:20}} onPress={this.find_idpwBtn}>
-                <Text  style={{fontSize:15,fontFamily:'Jalnan'}}>ID/PW 찾기</Text>
-              </TouchableOpacity>
-            </View>
+          <View style={styles.Text_login}>
+            <Text style={styles.Text_login_text}>비밀번호</Text>
+            <TextInput
+              style={styles.Text_login_input2}
+              id="pass"
+              name="pass"
+              value={this.state.pass}
+              secureTextEntry={true}
+              onChangeText={this.handleName2}
+            />
           </View>
+        </View>
 
-          <View style={{width: "100%"}}>
-            <TouchableOpacity style={styles.Btn_login} onPress={this.onlogin}>
-              <Text style={{color:'white',fontFamily:'Jalnan',fontSize:20}}>로그인</Text>
+        <View style={styles.bg3}>
+          <View style={styles.login_button_view}>
+            <TouchableOpacity
+              style={{marginRight: 70}}
+              onPress={this.singupBtn}>
+              <Text style={{fontSize: 15, fontFamily: 'Jalnan'}}>
+                처음이신가요?
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{marginRight: 20}}
+              onPress={this.find_idpwBtn}>
+              <Text style={{fontSize: 15, fontFamily: 'Jalnan'}}>
+                ID/PW 찾기
+              </Text>
             </TouchableOpacity>
           </View>
+        </View>
+
+        <View style={{width: '100%'}}>
+          <TouchableOpacity style={styles.Btn_login} onPress={this.onlogin}>
+            <Text style={{color: 'white', fontFamily: 'Jalnan', fontSize: 20}}>
+              로그인
+            </Text>
+          </TouchableOpacity>
+        </View> */}
       </SafeAreaView>
-    )
+    );
   }
 }
 
 const styles = StyleSheet.create({
-  login_button_view:{
-    display:"flex",
-    flexDirection:"row",
-    marginTop:20
+  login_button_view: {
+    display: 'flex',
+    flexDirection: 'row',
+    marginTop: 20,
   },
-  White_login:{
-    display: "flex",
-    alignItems: "center",
-    flex:1,
+  White_login: {
+    display: 'flex',
+    // alignItems: 'center',
+    flex: 1,
     //backgroundColor:"#c7d9ff"
-    backgroundColor:"white",
-    justifyContent: "space-between"
+    backgroundColor: 'white',
+    // justifyContent: 'space-between',
   },
-  Textbox_login:{
-    display:"flex",
-    flexDirection:"column",
-    justifyContent:"center",
-    alignItems:"center",
+  Textbox_login: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 
-  Swiper_bg:{
-    display:"flex",
+  Swiper_bg: {
+    display: 'flex',
     marginTop: 20,
-    height: "45%"
+    height: '45%',
   },
-  wrapper:{
-  },
+  wrapper: {},
   slide1: {
     flex: 1,
     alignItems: 'center',
@@ -227,82 +396,78 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
   },
-  swiper_image: {
-  },
+  swiper_image: {},
   swiper_text: {
     fontSize: 16,
-    fontWeight: "bold",
-    marginBottom: 7
+    fontWeight: 'bold',
+    marginBottom: 7,
   },
-  
-  Textbox_login2:{
-    display:"flex",
-    flexDirection:"column",
-    justifyContent:"center",
-    alignItems:"center",
-    marginTop:20
-   
+
+  Textbox_login2: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 20,
   },
-  Intro_login:{
-    marginTop:0,
-    fontSize:15,
-    color:"#f05052",
-    fontFamily:"Jalnan",
-    marginBottom:3
+  Intro_login: {
+    marginTop: 0,
+    fontSize: 15,
+    color: '#f05052',
+    fontFamily: 'Jalnan',
+    marginBottom: 3,
   },
-  Intro_login2:{
-    marginTop:0,
-    fontSize:30,
-    color:"#f05052",
-    fontFamily:"Jalnan"
+  Intro_login2: {
+    marginTop: 0,
+    fontSize: 30,
+    color: '#f05052',
+    fontFamily: 'Jalnan',
   },
-  Text_login:{
-    display:"flex",
-    flexDirection:'row',
-    width:'80%',
-    marginTop:25,
-    borderBottomWidth:1,
-    borderBottomColor:'gray'
+  Text_login: {
+    display: 'flex',
+    flexDirection: 'row',
+    width: '80%',
+    marginTop: 25,
+    borderBottomWidth: 1,
+    borderBottomColor: 'gray',
   },
-  Text_login_text:{
-    fontFamily:'Jalnan',
-    fontSize:15,
-    color:'#f05052',
+  Text_login_text: {
+    fontFamily: 'Jalnan',
+    fontSize: 15,
+    color: '#f05052',
   },
-  Text_login_input:{
-    display:"flex",
-    color:'black',
-    flex:0.9,
-    height:20,
-    fontSize:15,
-    marginLeft:20,
-    padding:0
-    
+  Text_login_input: {
+    display: 'flex',
+    color: 'black',
+    flex: 0.9,
+    height: 20,
+    fontSize: 15,
+    marginLeft: 20,
+    padding: 0,
   },
-  Text_login_input2:{
-    display:"flex",
-    color:'black',
-    flex:0.9,
-    height:20,
-    fontSize:15,
-    marginLeft:10,
-    padding:0
-    
+  Text_login_input2: {
+    display: 'flex',
+    color: 'black',
+    flex: 0.9,
+    height: 20,
+    fontSize: 15,
+    marginLeft: 10,
+    padding: 0,
   },
-  Btn_login:{
-    borderWidth:0,
-    marginTop:20,
-    color:'white',
-    fontFamily:'Jalnan',
-    alignItems: "center",
-    paddingLeft:30,
-    paddingTop:10,
-    paddingRight:30,
-    paddingBottom:10,
-    fontSize:20,
-    backgroundColor:'#f05052',
-    elevation:8,
-    marginBottom:10
-  }
+  Btn_login: {
+    borderWidth: 0,
+    marginTop: 20,
+    color: 'white',
+    fontFamily: 'Jalnan',
+    alignItems: 'center',
+    paddingLeft: 30,
+    paddingTop: 10,
+    paddingRight: 30,
+    paddingBottom: 10,
+    fontSize: 20,
+    backgroundColor: '#f05052',
+    elevation: 8,
+    marginBottom: 10,
+  },
 });
 export default withNavigation(Login);

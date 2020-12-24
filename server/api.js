966ddc2d67050521e6f9f8bf47ea89a030738c40
정영빈 <jybin96@changwon.com>
2020-12-24 13:38:40 +0900

    asdad

--- server/api.js
+++ server/api.js
@@ -1,6 +1,6 @@
 
 function api(params, name) {
-  return 'http://192.168.1.120:' + `${params}` + `/${name}`;
+  return 'http://192.168.1.113:' + `${params}` + `/${name}`;
 }
 module.exports = {
   api,

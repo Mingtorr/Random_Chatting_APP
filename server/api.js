d84299bb60695d51ecff57b7a99277056b1d4b63
iamhoon <zown98@naver.com>
2021-01-14 13:22:15 +0900

    api수정

--- server/api.js
+++ server/api.js
@@ -1,12 +1,6 @@
 function api(params, name) {
   return 'http://34.64.151.54:' + `${params}` + `/${name}`;
-<<<<<<< HEAD
-  // return 'http://192.168.0.4:' + `${params}` + `/${name}`;
-  // return 'http://192.168.1.132:' + `${params}` + `/${name}`;
-  
-=======
   // return 'http://192.168.1.110:' + `${params}` + `/${name}`;
->>>>>>> master
 }
 module.exports = {
   api,

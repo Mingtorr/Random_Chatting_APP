d538a7d4895b5a03a28c30494c0e61f7f8012eb2
Won-young <dnjsdud2257@gmail.com>
2021-01-18 20:29:01 +0900

    전체 채팅방 링크 및 텍스트 복사 추가

--- main/main_mymessage.js
+++ main/main_mymessage.js
@@ -1,42 +1,56 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, Text, Linking, Clipboard, TouchableOpacity, Alert} from 'react-native';
 
 export default class Main_Mymessage extends React.PureComponent {
+  copyToClipboard = () => {
+    Clipboard.setString(this.props.message)
+    Alert.alert('','텍스트가 복사되었습니다.')
+  }
+  
   render() {
     return (
-      <View
-        style={{
-          display: 'flex',
-          flexDirection: 'row',
-          justifyContent: 'flex-end',
-          marginBottom: 5,
-          marginTop: 5,
-        }}>
-        <View
-          style={{
-            display: 'flex',
-            justifyContent: 'flex-end',
-            marginRight: 10,
-          }}>
-          <Text style={{fontSize: 10}}>{this.props.time}</Text>
-        </View>
-        <View
-          style={{
-            display: 'flex',
-            backgroundColor: '#eaeaea',
-            marginRight: 16,
-            maxWidth: '70%',
-            paddingLeft: 15,
-            paddingRight: 15,
-            paddingBottom: 8,
-            paddingTop: 8,
-            borderRadius: 20,
-          }}>
-          {/* <Text style={{color: this.props.animate_boolean ? 'black': 'white', fontSize: 14}}>{this.props.message}</Text> */}
-          <Text style={{color: 'black', fontSize: 14}}>
-            {this.props.message}
-          </Text>
-        </View>
+      <View>
+        <TouchableOpacity onLongPress = {this.copyToClipboard}>
+          <View
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'flex-end',
+              marginBottom: 5,
+              marginTop: 5,
+            }}>
+            <View
+              style={{
+                display: 'flex',
+                justifyContent: 'flex-end',
+                marginRight: 10,
+              }}>
+              <Text style={{fontSize: 10}}>{this.props.time}</Text>
+            </View>
+            <View
+              style={{
+                display: 'flex',
+                backgroundColor: '#eaeaea',
+                marginRight: 16,
+                maxWidth: '70%',
+                paddingLeft: 15,
+                paddingRight: 15,
+                paddingBottom: 8,
+                paddingTop: 8,
+                borderRadius: 20,
+              }}>
+              {/* <Text style={{color: this.props.animate_boolean ? 'black': 'white', fontSize: 14}}>{this.props.message}</Text> */}
+              {this.props.message.includes('http://') || this.props.message.includes('https://') // 링크접속하는곳
+              ?<Text style={{color: 'blue'}}
+                onPress={() => Linking.openURL(this.props.message)}>
+                {this.props.message}
+              </Text>
+              :<Text style={{fontSize: 14, color: 'black'}}>
+                {this.props.message}
+              </Text> }
+            </View>
+          </View>
+        </TouchableOpacity>
       </View>
     );
   }
--- main/main_yourmessage.js
+++ main/main_yourmessage.js
@@ -1,39 +1,55 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, Text, Linking, Clipboard, TouchableOpacity, Alert} from 'react-native';
 
 export default class Main_Yourmessage extends React.PureComponent {
+  copyToClipboard = () => {
+    Clipboard.setString(this.props.message)
+    Alert.alert('','텍스트가 복사되었습니다.')
+  }
+
   render() {
     if (this.props.pre === true) {
       return (
-        <View
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'flex-start',
-            marginBottom: 5,
-            marginTop: 5,
-          }}>
-          <View style={{display: 'flex', flexDirection: 'row'}}>
+        <View>
+          <TouchableOpacity onLongPress = {this.copyToClipboard}>
             <View
               style={{
                 display: 'flex',
-                backgroundColor: '#eaeaea',
-                // backgroundColor: '#eb6c63',
-                marginRight: 10,
-                marginLeft: 16,
-                maxWidth: '70%',
-                paddingLeft: 20,
-                paddingRight: 20,
-                paddingBottom: 10,
-                paddingTop: 10,
-                borderRadius: 28,
+                flexDirection: 'column',
+                justifyContent: 'flex-start',
+                marginBottom: 5,
+                marginTop: 5,
               }}>
-              <Text style={{fontSize: 18}}>{this.props.message}</Text>
+              <View style={{display: 'flex', flexDirection: 'row'}}>
+                <View
+                  style={{
+                    display: 'flex',
+                    backgroundColor: '#eaeaea',
+                    // backgroundColor: '#eb6c63',
+                    marginRight: 10,
+                    marginLeft: 16,
+                    maxWidth: '70%',
+                    paddingLeft: 20,
+                    paddingRight: 20,
+                    paddingBottom: 10,
+                    paddingTop: 10,
+                    borderRadius: 28,
+                  }}>
+                  {this.props.message.includes('http://') || this.props.message.includes('https://') // 링크접속하는곳
+                  ?<Text style={{color: 'blue'}}
+                    onPress={() => Linking.openURL(this.props.message)}>
+                    {this.props.message}
+                  </Text>
+                  :<Text style={{fontSize: 14, color: 'white'}}>
+                    {this.props.message}
+                  </Text> }
+                </View>
+                <View style={{display: 'flex', justifyContent: 'flex-end'}}>
+                  <Text style={{fontSize: 12}}>{this.props.time}</Text>
+                </View>
+              </View>
             </View>
-            <View style={{display: 'flex', justifyContent: 'flex-end'}}>
-              <Text style={{fontSize: 12}}>{this.props.time}</Text>
-            </View>
-          </View>
+          </TouchableOpacity>
         </View>
       );
     } else {
@@ -51,29 +67,37 @@ export default class Main_Yourmessage extends React.PureComponent {
               {this.props.nickname}
             </Text>
           </View>
-          <View style={{display: 'flex', flexDirection: 'row'}}>
-            <View
-              style={{
-                display: 'flex',
-                // backgroundColor: '#c7d9ff',
-                backgroundColor: '#eb6c63',
-                marginRight: 10,
-                marginLeft: 16,
-                maxWidth: '70%',
-                paddingLeft: 15,
-                paddingRight: 15,
-                paddingBottom: 8,
-                paddingTop: 8,
-                borderRadius: 20,
-              }}>
-              <Text style={{fontSize: 14, color: 'white'}}>
-                {this.props.message}
-              </Text>
+
+          <TouchableOpacity onLongPress = {this.copyToClipboard}>
+            <View style={{display: 'flex', flexDirection: 'row'}}>
+              <View
+                style={{
+                  display: 'flex',
+                  // backgroundColor: '#c7d9ff',
+                  backgroundColor: '#eb6c63',
+                  marginRight: 10,
+                  marginLeft: 16,
+                  maxWidth: '70%',
+                  paddingLeft: 15,
+                  paddingRight: 15,
+                  paddingBottom: 8,
+                  paddingTop: 8,
+                  borderRadius: 20,
+                }}>
+                {this.props.message.includes('http://') || this.props.message.includes('https://') // 링크접속하는곳
+                  ?<Text style={{color: 'blue'}}
+                    onPress={() => Linking.openURL(this.props.message)}>
+                    {this.props.message}
+                  </Text>
+                  :<Text style={{fontSize: 14, color: 'white'}}>
+                    {this.props.message}
+                  </Text> }
+              </View>
+              <View style={{display: 'flex', justifyContent: 'flex-end'}}>
+                <Text style={{fontSize: 10}}>{this.props.time}</Text>
+              </View>
             </View>
-            <View style={{display: 'flex', justifyContent: 'flex-end'}}>
-              <Text style={{fontSize: 10}}>{this.props.time}</Text>
-            </View>
-          </View>
+          </TouchableOpacity>
         </View>
       );
     }

0bd64e8c8c8ac95ea9b9a7ba712b38e63f2789ac
jybin96 <jybin96@changwon.ac.kr>
2021-01-20 01:04:42 +0900

    차단오류 수정

--- server/api.js
+++ server/api.js
@@ -1,6 +1,6 @@
 function api(params, name) {
-  //return 'http://34.64.151.54:' + `${params}` + `/${name}`;
-   return 'http://192.168.0.6:' + `${params}` + `/${name}`;
+  return 'http://34.64.151.54:' + `${params}` + `/${name}`;
+   //return 'http://172.20.10.2:' + `${params}` + `/${name}`;
 }
 module.exports = {
   api,
